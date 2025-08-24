1305b17200e383a80a872ad7b979a78adab01ee8
zillion-ai <zillion.ai.2000@gmail.com>
2025-08-24 14:59:26 +0800

    Add ios android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ai.zillion',
+  appName: 'zillion.ai',
+  webDir: 'build'
+};
+
+export default config;
